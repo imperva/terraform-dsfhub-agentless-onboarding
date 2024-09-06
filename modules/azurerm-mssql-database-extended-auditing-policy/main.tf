67806b98488faa48e0679fd70dcb7028cdde07d3
mattJsonar <matt.gill@imperva.com>
2024-09-06 14:06:19 -0700

    remove database extended policy

--- modules/azurerm-mssql-database-extended-auditing-policy/main.tf
+++
@@ -1,7 +0,0 @@
-resource "azurerm_mssql_database_extended_auditing_policy" "this" {
-  database_id                = var.database_id
-  enabled                    = var.enabled
-  storage_account_access_key = var.storage_account_access_key
-  storage_endpoint           = var.storage_endpoint
-  retention_in_days          = var.retention_in_days
-}
--- modules/azurerm-mssql-database-extended-auditing-policy/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "this" {
-  description = "Azure SQL Database Extended Auditing Policy."
-  value       = azurerm_mssql_database_extended_auditing_policy.this
-}
