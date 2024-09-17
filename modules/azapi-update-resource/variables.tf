bd39aef429d5d10e8e6906086880c76468581508
mattJsonar <matt.gill@imperva.com>
2024-09-17 12:43:53 -0700

    remove angle brackets

--- modules/azapi-update-resource/variables.tf
+++ modules/azapi-update-resource/variables.tf
@@ -1,5 +1,5 @@
 variable "body" {
-  description = "It is in a format like <resource-type>@<api-version>. <resource-type> is the Azure resource type, for example, Microsoft.Storage/storageAccounts. <api-version> is version of the API used to manage this azure resource."
+  description = "It is in a format like resource-type@api-version. resource-type is the Azure resource type, for example, Microsoft.Storage/storageAccounts. api-version is version of the API used to manage this azure resource."
   type        = string
 }
 
