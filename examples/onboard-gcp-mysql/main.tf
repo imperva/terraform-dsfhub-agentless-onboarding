4fac2e5115b06abf6f75df047ab20d1d82916c20
mattJsonar <matt.gill@imperva.com>
2024-11-14 12:10:44 -0800

    refactor examples

--- examples/onboard-gcp-mysql/main.tf
+++ examples/onboard-gcp-mysql/main.tf
@@ -41,7 +41,7 @@ provider "dsfhub" {}
 ################################################################################
 # Prerequisites
 # 1. A service account with permissions to read from the PubSub subscription
-# 2. A Google sink router, PubSub topic and subscription
+# 2. A Google sink router, PubSub topic and subscription (handled below)
 ################################################################################
 module "service-account" {
   source = "../../modules/google-service-account-dsf"
@@ -56,6 +56,13 @@ module "service-account" {
   ]
 }
 
+################################################################################
+# GCP MySQL 8.0
+################################################################################
+locals {
+  gcp_mysql_1_instance_name = "tf-mysql-8"
+}
+
 module "gcp-pubsub-1" {
   source = "../../modules/onboard-gcp-pubsub"
 
@@ -66,18 +73,49 @@ module "gcp-pubsub-1" {
 
   project = local.gcp_project_id
 
-  pubsub_subscription_name = "tf-mysql-sub-1"
+  pubsub_subscription_name = "${local.gcp_mysql_1_instance_name}-sub"
 
-  pubsub_topic_name = "tf-mysql-topic-1"
+  pubsub_topic_name = "${local.gcp_mysql_1_instance_name}-topic"
 
   sink_router_description = "MySQL 8.0 sink"
   sink_router_exclusions  = local.excluded_traffic_filter
   sink_router_filter      = <<EOF
     resource.type="cloudsql_database"
-    resource.labels.database_id="${local.gcp_project_id}:tf-mysql-8"
+    resource.labels.database_id="${local.gcp_project_id}:${local.gcp_mysql_1_instance_name}"
     logName="projects/${local.gcp_project_id}/logs/cloudsql.googleapis.com%2Fmysql-general.log"
   EOF
-  sink_router_name        = "tf-mysql-sink-1"
+  sink_router_name        = "${local.gcp_mysql_1_instance_name}-sink"
+}
+
+module "gcp-mysql-1" {
+  source = "../../modules/onboard-gcp-mysql"
+
+  gcp_mysql_admin_email               = local.admin_email
+  gcp_mysql_audit_pull_enabled        = true
+  gcp_mysql_gateway_id                = local.gateway_id
+  gcp_mysql_logs_destination_asset_id = module.gcp-pubsub-1.gcp-pubsub-asset.asset_id
+
+  instance_authorized_networks = local.gcp_mysql_instance_authorized_networks
+  instance_database_version    = "MYSQL_8_0"
+  instance_name                = local.gcp_mysql_1_instance_name
+  instance_region              = "us-west1"
+}
+
+################################################################################
+# GCP MySQL 5.7 with slow query monitoring
+#
+# Notes:
+# The Pubsub containing audit data will be associated with the GCP MySQL asset
+# via the "logs_destination_asset_id" and will get connected to gateway when the
+# database asset does.
+# 
+# The Pubsub containing slow query data will be treated as a "standalone" pubsub
+# and will get connected to gateway by setting the "audit_pull_enabled" field to
+# true directly on the asset. It utilizes the "content_type" field to indicate
+# that the data within the pubsub is coming from a GCP MySQL instance.
+################################################################################
+locals {
+  gcp_mysql_2_instance_name = "tf-mysql-5-7-slow-query"
 }
 
 module "gcp-pubsub-2-audit" {
@@ -90,18 +128,18 @@ module "gcp-pubsub-2-audit" {
 
   project = local.gcp_project_id
 
-  pubsub_subscription_name = "tf-mysql-sub-2-audit"
+  pubsub_subscription_name = "${local.gcp_mysql_2_instance_name}-audit-sub"
 
-  pubsub_topic_name = "tf-mysql-topic-2-audit"
+  pubsub_topic_name = "${local.gcp_mysql_2_instance_name}-audit-topic"
 
   sink_router_description = "MySQL 5.7 audit log sink"
   sink_router_exclusions  = local.excluded_traffic_filter
   sink_router_filter      = <<EOF
     resource.type="cloudsql_database"
-    resource.labels.database_id="${local.gcp_project_id}:tf-mysql-5-7-slow-query"
+    resource.labels.database_id="${local.gcp_project_id}:${local.gcp_mysql_2_instance_name}"
     logName="projects/${local.gcp_project_id}/logs/cloudsql.googleapis.com%2Fmysql-general.log"
   EOF
-  sink_router_name        = "tf-mysql-sink-2-audit"
+  sink_router_name        = "${local.gcp_mysql_2_instance_name}-audit-sink"
 }
 
 module "gcp-pubsub-2-slow-query" {
@@ -116,66 +154,20 @@ module "gcp-pubsub-2-slow-query" {
 
   project = local.gcp_project_id
 
-  pubsub_subscription_name = "tf-mysql-sub-2-slow-query"
+  pubsub_subscription_name = "${local.gcp_mysql_2_instance_name}-slow-query-sub"
 
-  pubsub_topic_name = "tf-mysql-topic-2-slow-query"
+  pubsub_topic_name = "${local.gcp_mysql_2_instance_name}-slow-query-topic"
 
   sink_router_description = "MySQL 5.7 slow query sink"
   sink_router_exclusions  = local.excluded_traffic_filter
   sink_router_filter      = <<EOF
     resource.type="cloudsql_database"
-    resource.labels.database_id="${local.gcp_project_id}:tf-mysql-5-7-slow-query"
+    resource.labels.database_id="${local.gcp_project_id}:${local.gcp_mysql_2_instance_name}"
     logName="projects/${local.gcp_project_id}/logs/cloudsql.googleapis.com%2Fmysql-slow.log"
   EOF
-  sink_router_name        = "tf-mysql-sink-2-slow-query"
+  sink_router_name        = "${local.gcp_mysql_2_instance_name}-slow-query-sink"
 }
 
-module "gcp-pubsub-3" {
-  source = "../../modules/onboard-gcp-pubsub"
-
-  gcp_pubsub_admin_email    = local.admin_email
-  gcp_pubsub_audit_type     = "MYSQL"
-  gcp_pubsub_auth_mechanism = local.pubsub_auth_mechanism
-  gcp_pubsub_gateway_id     = local.gateway_id
-
-  project = local.gcp_project_id
-
-  pubsub_subscription_name = "tf-mysql-sub-3"
-
-  pubsub_topic_name = "tf-mysql-topic-3"
-
-  sink_router_description = "MySQL 8.0 many-to-one sink"
-  sink_router_exclusions  = local.excluded_traffic_filter
-  sink_router_filter      = <<EOF
-    resource.type="cloudsql_database"
-    resource.labels.database_id="${local.gcp_project_id}:tf-mysql-dev" OR 
-    resource.labels.database_id="${local.gcp_project_id}:tf-mysql-prod" OR 
-    resource.labels.database_id="${local.gcp_project_id}:tf-mysql-uat"
-    logName="projects/${local.gcp_project_id}/logs/cloudsql.googleapis.com%2Fmysql-general.log"
-  EOF
-  sink_router_name        = "tf-mysql-sink-3"
-}
-
-################################################################################
-# GCP MySQL 8.0
-################################################################################
-module "gcp-mysql-1" {
-  source = "../../modules/onboard-gcp-mysql"
-
-  gcp_mysql_admin_email               = local.admin_email
-  gcp_mysql_audit_pull_enabled        = true
-  gcp_mysql_gateway_id                = local.gateway_id
-  gcp_mysql_logs_destination_asset_id = module.gcp-pubsub-1.gcp-pubsub-asset.asset_id
-
-  instance_authorized_networks = local.gcp_mysql_instance_authorized_networks
-  instance_database_version    = "MYSQL_8_0"
-  instance_name                = "tf-mysql-8"
-  instance_region              = "us-west1"
-}
-
-################################################################################
-# GCP MySQL 5.7 with slow query monitoring
-################################################################################
 module "gcp-mysql-2" {
   source = "../../modules/onboard-gcp-mysql"
 
@@ -204,7 +196,7 @@ module "gcp-mysql-2" {
       "value" : "10"
     }
   ]
-  instance_name   = "tf-mysql-5-7-slow-query"
+  instance_name   = local.gcp_mysql_2_instance_name
   instance_region = "us-west1"
 }
 
@@ -212,17 +204,63 @@ module "gcp-mysql-2" {
 # GCP MySQL 8.0 Many-to-one
 ################################################################################
 locals {
-  mysql_types = toset([
+  gcp_mysql_3_instance_name_prefix = "tf-mysql-multi"
+  gcp_mysql_3_instance_types = toset([
     "dev",
     "prod",
-    "uat"
+    "uat",
   ])
+
+  # Build instance names
+  gcp_mysql_3_instance_names = toset([
+    for i, type in local.gcp_mysql_3_instance_types : "${local.gcp_mysql_3_instance_name_prefix}-${type}"
+  ])
+
+  # Build database IDs
+  gcp_mysql_3_database_ids = [
+    for i, name in local.gcp_mysql_3_instance_names : "\"${local.gcp_project_id}:${name}\""
+  ]
+
+  # Build sink filter
+  gcp_mysql_3_filter = join(
+    "",
+    [
+      "resource.type=\"cloudsql_database\"\n",
+      "logName=\"projects/${local.gcp_project_id}/logs/cloudsql.googleapis.com%2Fmysql-general.log\"\n",
+      "resource.labels.database_id=(",
+      join(
+        " OR ",
+        local.gcp_mysql_3_database_ids
+      ),
+      ")",
+    ]
+  )
+}
+
+module "gcp-pubsub-3" {
+  source = "../../modules/onboard-gcp-pubsub"
+
+  gcp_pubsub_admin_email    = local.admin_email
+  gcp_pubsub_audit_type     = "MYSQL"
+  gcp_pubsub_auth_mechanism = local.pubsub_auth_mechanism
+  gcp_pubsub_gateway_id     = local.gateway_id
+
+  project = local.gcp_project_id
+
+  pubsub_subscription_name = "${local.gcp_mysql_3_instance_name_prefix}-sub"
+
+  pubsub_topic_name = "${local.gcp_mysql_3_instance_name_prefix}-topic"
+
+  sink_router_description = "MySQL 8.0 many-to-one sink"
+  sink_router_exclusions  = local.excluded_traffic_filter
+  sink_router_filter      = local.gcp_mysql_3_filter
+  sink_router_name        = "${local.gcp_mysql_3_instance_name_prefix}-sink"
 }
 
 module "gcp-mysql-3" {
   source = "../../modules/onboard-gcp-mysql"
 
-  for_each = local.mysql_types
+  for_each = local.gcp_mysql_3_instance_names
 
   gcp_mysql_admin_email               = local.admin_email
   gcp_mysql_audit_pull_enabled        = true
@@ -231,6 +269,6 @@ module "gcp-mysql-3" {
 
   instance_authorized_networks = local.gcp_mysql_instance_authorized_networks
   instance_database_version    = "MYSQL_8_0"
-  instance_name                = "tf-mysql-${each.key}"
+  instance_name                = each.key
   instance_region              = "us-west1"
 }
