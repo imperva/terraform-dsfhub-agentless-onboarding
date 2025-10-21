c4a63c0c35d8370977ebcd89c5c00064a307322c
Petal Vitis <petal.vitis@imperva.com>
2025-10-21 16:26:03 -0700

    commented out audit_data_type

--- modules/dsfhub-gcp-cloud-storage-bucket/main.tf
+++ modules/dsfhub-gcp-cloud-storage-bucket/main.tf
@@ -13,14 +13,14 @@ resource "dsfhub_log_aggregator" "this" {
   asset_display_name = var.asset_display_name
   asset_id           = var.asset_id
   audit_pull_enabled = var.audit_pull_enabled
-  audit_data_type    = var.audit_data_type
-  audit_type         = var.audit_type
-  content_type       = var.content_type
-  gateway_id         = var.gateway_id
-  parent_asset_id    = var.parent_asset_id
-  server_host_name   = "storage.googleapis.com"
-  server_ip          = "storage.googleapis.com"
-  server_port        = "443"
+  # audit_data_type    = var.audit_data_type
+  audit_type       = var.audit_type
+  content_type     = var.content_type
+  gateway_id       = var.gateway_id
+  parent_asset_id  = var.parent_asset_id
+  server_host_name = "storage.googleapis.com"
+  server_ip        = "storage.googleapis.com"
+  server_port      = "443"
 
   asset_connection {
     auth_mechanism = "default"
